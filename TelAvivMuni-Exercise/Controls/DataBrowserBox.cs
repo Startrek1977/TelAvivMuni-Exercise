eeaf172bd4bb848f5f8f6657dfe83bbd23decc94
Startrek1977 <iro1977@gmail.com>
2026-01-09 09:53:13 +0200

    Dependency properties have been added. Placeholders in MW were replaced by the real controls.

--- TelAvivMuni-Exercise/Controls/DataBrowserBox.cs
+++ TelAvivMuni-Exercise/Controls/DataBrowserBox.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -5,6 +6,46 @@ namespace TelAvivMuni_Exercise.Controls
 {
     public class DataBrowserBox : Control
     {
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register(nameof(ItemsSource), typeof(IEnumerable), typeof(DataBrowserBox),
+                new PropertyMetadata(null));
+
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register(nameof(SelectedItem), typeof(object), typeof(DataBrowserBox),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty DisplayMemberPathProperty =
+            DependencyProperty.Register(nameof(DisplayMemberPath), typeof(string), typeof(DataBrowserBox),
+                new PropertyMetadata(null));
+
+        public static readonly DependencyProperty DialogTitleProperty =
+            DependencyProperty.Register(nameof(DialogTitle), typeof(string), typeof(DataBrowserBox),
+                new PropertyMetadata(null));
+
+        public IEnumerable ItemsSource
+        {
+            get => (IEnumerable)GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
+
+        public object SelectedItem
+        {
+            get => GetValue(SelectedItemProperty);
+            set => SetValue(SelectedItemProperty, value);
+        }
+
+        public string DisplayMemberPath
+        {
+            get => (string)GetValue(DisplayMemberPathProperty);
+            set => SetValue(DisplayMemberPathProperty, value);
+        }
+
+        public string DialogTitle
+        {
+            get => (string)GetValue(DialogTitleProperty);
+            set => SetValue(DialogTitleProperty, value);
+        }
+
         static DataBrowserBox()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(DataBrowserBox),
